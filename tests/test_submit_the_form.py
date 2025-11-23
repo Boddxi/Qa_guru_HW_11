a047251f075a6b4f3f4ec07c0d9e8483f6acbf33
Juliya <julieta-petrova@yandex.ru>
2025-11-23 17:18:27 +0300

    добавила Allure шаги в тесте

--- tests/test_submit_the_form.py
+++ tests/test_submit_the_form.py
@@ -1,10 +1,20 @@
+import allure
 from pages.registration_page import RegistrationPage
-
 from data.users import student
 
-
+@allure.feature("Регистрация пользователя")
 def test_student_registration_form(browser_management):
     registration_page = RegistrationPage()
-    registration_page.open()
-    registration_page.register(student)
-    registration_page.should_have_registered(student)
+
+    with allure.step('Открыть страницу регистрации'):
+        registration_page.open()
+
+    with allure.step('Зарегистрировать пользователя'):
+        registration_page.register(student)
+
+    with allure.step('Проверить зарегистрирован ли пользователь'):
+        registration_page.should_have_registered(student)
+
+
+
+
