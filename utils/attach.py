56ad6ce0d117a577e14670806992aa7a6475eaca
Juliya Smikova <boddxi@yandex.ru>
2025-11-25 14:32:01 +0300

    изменила conftest.py

--- utils/attach.py
+++ utils/attach.py
@@ -21,11 +21,9 @@ def add_html(browser):
 
 
 # видео
-def add_video(driver):
-    video_url = f"https://selenoid.autotests.cloud/video/{driver.session_id}.mp4"
-    html = (
-        "<html><body><video width='100%' height='100%' controls autoplay>"
-        f"<source src='{video_url}' type='video/mp4'></video></body></html>"
-    )
-
-    allure.attach(html, f"video_{driver.session_id}", AttachmentType.HTML, ".html")
+def add_video(browser):
+    video_url = "https://selenoid.autotests.cloud/video/" + browser.driver.session_id + ".mp4"
+    html = "<html><body><video width='100%' height='100%' controls autoplay><source src='" \
+           + video_url \
+           + "' type='video/mp4'></video></body></html>"
+    allure.attach(html, 'video_' + browser.driver.session_id, AttachmentType.HTML, '.html')
