14322e2114dbe475457dfc3cb365e2c1699be5e8
Juliya Smikova <boddxi@yandex.ru>
2025-11-25 13:45:44 +0300

    изменила conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,49 @@
 import pytest
 from selene import browser
+from dotenv import load_dotenv
+import os
+from utils import attach
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope="function")
 def browser_management():
-    browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_height = 1080
-    browser.config.window_width = 1920
-    yield
-    browser.quit()
+    options = Options()
+
+    options.set_capability("browserName", "chrome")
+    options.set_capability("browserVersion", "128.0")
+    options.set_capability("selenoid:options", {
+        "enableVNC": True,
+        "enableVideo": True
+    })
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    # browser.config.base_url = 'https://demoqa.com'
+    # browser.config.window_height = 1080
+    # browser.config.window_width = 1920
+
+    yield driver
+
+    attach.add_screenshot(driver)
+    attach.add_logs(driver)
+    attach.add_html(driver)
+    attach.add_video(driver)
+
+    driver.quit()
+
+
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+
+selenoid_login = os.getenv("SELENOID_LOGIN")
+selenoid_pass = os.getenv("SELENOID_PASS")
+selenoid_url = os.getenv("SELENOID_URL")
+
